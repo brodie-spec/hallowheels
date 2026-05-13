450fbd1e6ecf57bbe7ad0121a25d35d780b46e3d
tattonchantry <brodiewells@msn.com>
2026-05-13 19:57:37 -0400

    Fix Neon Form

--- components/NeonForm.js
+++ components/NeonForm.js
@@ -1,37 +1,14 @@
 'use client'
 
-import { useEffect, useRef } from 'react'
-
-const NEON_SCRIPT_URL = 'https://atdevicesforkids.app.neoncrm.com/forms/share/Rk9STS1FTUJFRFNIQVJJTkctQ09ERDE4'
+import Script from 'next/script'
 
 export default function NeonForm() {
-  const containerRef = useRef(null)
-  const loadedRef    = useRef(false)
-
-  useEffect(() => {
-    if (loadedRef.current) return
-    loadedRef.current = true
-
-    const script = document.createElement('script')
-    script.src  = NEON_SCRIPT_URL
-    script.async = true
-
-    if (containerRef.current) {
-      containerRef.current.appendChild(script)
-    }
-
-    return () => {
-      if (containerRef.current && script.parentNode === containerRef.current) {
-        containerRef.current.removeChild(script)
-      }
-    }
-  }, [])
-
   return (
-    <div
-      ref={containerRef}
-      aria-label="Sponsorship donation form"
-      style={{ minHeight: '400px' }}
-    />
+    <div aria-label="Sponsorship donation form" style={{ minHeight: '400px' }}>
+      <Script
+        src="https://atdevicesforkids.app.neoncrm.com/forms/share/Rk9STS1FTUJFRFNIQVJJTkctQ09ERDE4"
+        strategy="lazyOnload"
+      />
+    </div>
   )
 }
