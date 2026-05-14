8f707a59ce427e158a8d197b3b8b1b4f07aaee12
tattonchantry <brodiewells@msn.com>
2026-05-13 20:01:33 -0400

    Fix Neon Form again

--- components/NeonForm.js
+++ components/NeonForm.js
@@ -1,14 +1,12 @@
-'use client'
-
-import Script from 'next/script'
-
 export default function NeonForm() {
   return (
-    <div aria-label="Sponsorship donation form" style={{ minHeight: '400px' }}>
-      <Script
-        src="https://atdevicesforkids.app.neoncrm.com/forms/share/Rk9STS1FTUJFRFNIQVJJTkctQ09ERDE4"
-        strategy="lazyOnload"
-      />
-    </div>
+    <iframe
+      src="https://atdevicesforkids.app.neoncrm.com/forms/hallowheels2026"
+      width="100%"
+      height="900"
+      title="HalloWheels 2026 Sponsorship Donation Form"
+      aria-label="Secure sponsorship donation form powered by Neon CRM"
+      style={{ border: 'none', borderRadius: '8px' }}
+    />
   )
 }
